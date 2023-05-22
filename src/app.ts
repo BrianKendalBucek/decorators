0609ec8e77419759ec06f07cff035735de5eff2e
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-22 13:11:30 -0700

    example of property decorator

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,13 @@ console.log(pers)
 
 // ---
 
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator!');
+  console.log(target, propertyName);
+}
+
 class Product {
+  @Log
   title: string;
   private _price: number;
 
