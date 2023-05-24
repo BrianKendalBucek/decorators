3e926bf99e60c1b88de80be9601d4c1922604547
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-23 20:28:49 -0700

    added logic to for that stores data

--- src/app.ts
+++ src/app.ts
@@ -135,3 +135,15 @@ class Course {
   }
 }
 
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+  
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  console.log(createdCourse);
+});
